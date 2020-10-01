82e74f76c9b23a4498830e702a807bd567aabd8e
Ahmed-Ali-0 <71400128+Ahmed-Ali-0@users.noreply.github.com>
2020-10-01 12:45:08 -0400

    Add servo program

--- main.ts
+++ main.ts
@@ -2,6 +2,10 @@
 // Created Date: Oct 1, Thursday
 // 
 // This program used Servo motore
+basic.showIcon(IconNames.Happy)
 basic.forever(function () {
-	
+    robotbit.Servo(robotbit.Servos.S1, 57)
+    basic.pause(1000)
+    robotbit.Servo(robotbit.Servos.S1, 180)
+    basic.pause(1000)
 })
